1e46d700d9f4d439b060f0b63c293de65b8ed91e
Satyam-code143 <Satyam-code143@github.com>
2020-08-10 13:35:01 +0530

    trial 4

--- src/Components/Events/SystemEvents.jsx
+++ src/Components/Events/SystemEvents.jsx
@@ -56,66 +56,71 @@ const useStyles = (theme) => ({
 class SystemEvents extends Component {
   state = {
     finalArray: [],
-    isOpened:false
+    isOpened: false,
   };
-  componentDidMount(){
-   this.setState({isOpened:true})
-
+  componentDidMount = () => {
+    // this.setState({ isOpened: true });
+    this.setState({finalArray:[1,2,3]})
   };
-  componentDidUpdate(){
-    const { classes } = this.props;
-    var myArray1 = [];
-    var myArray2 = [];
 
-    for (let i = 0; i < Event.length; i++) {
-      myArray1.push(
-        <Grid item key={Event[i].id} lg>
-          <Paper
-            variant="elevation"
-            elevation={3}
-            className={classes.paperContainer}
-          >
-            <Typography component="div" className={classes.imageContainer}>
-              <Typography
-                component="img"
-                src={require(`../../Assets/${Event[i].image}.jpg`)}
-                className={classes.imageStyle}
-              />
-            </Typography>
-            <Typography component="div" className={classes.contentContainer}>
-              <Typography variant="h5" className={classes.subHeading}>
-                {Event[i].name}
-              </Typography>
-              <Divider />
-              <Typography component="div">
-                <Typography paragraph className={classes.description}>
-                  {Event[i].description}
-                </Typography>
-              </Typography>
-              <Button
-                variant="outlined"
-                color="default"
-                className={classes.buttonStyle}
-              >
-                Check Out
-              </Button>
-            </Typography>
-          </Paper>
-        </Grid>
-      );
-    }
-    for (let j = 0; j < Event.length; j += 3) {
-      myArray2.push(
-        <Grid component="div" className={classes.mainContainer} key={j}>
-          {myArray1[j]}
-          {myArray1[j + 1]}
-          {myArray1[j + 2]}
-        </Grid>
-      );
-    }
-    this.setState({ finalArray: myArray2 });
-  }
+  // componentDidUpdate(prevState) {
+  //   const { classes } = this.props;
+  //   var myArray1 = [];
+  //   var myArray2 = [];
 
+  //   for (let i = 0; i < Event.length; i++) {
+  //     myArray1.push(
+  //       <Grid item key={Event[i].id} lg>
+  //         <Paper
+  //           variant="elevation"
+  //           elevation={3}
+  //           className={classes.paperContainer}
+  //         >
+  //           <Typography component="div" className={classes.imageContainer}>
+  //             <Typography
+  //               component="img"
+  //               src={require(`../../Assets/${Event[i].image}.jpg`)}
+  //               className={classes.imageStyle}
+  //             />
+  //           </Typography>
+  //           <Typography component="div" className={classes.contentContainer}>
+  //             <Typography variant="h5" className={classes.subHeading}>
+  //               {Event[i].name}
+  //             </Typography>
+  //             <Divider />
+  //             <Typography component="div">
+  //               <Typography paragraph className={classes.description}>
+  //                 {Event[i].description}
+  //               </Typography>
+  //             </Typography>
+  //             <Button
+  //               variant="outlined"
+  //               color="default"
+  //               className={classes.buttonStyle}
+  //             >
+  //               Check Out
+  //             </Button>
+  //           </Typography>
+  //         </Paper>
+  //       </Grid>
+  //     );
+  //   }
+  //   for (let j = 0; j < Event.length; j += 3) {
+  //     myArray2.push(
+  //       <Grid component="div" className={classes.mainContainer} key={j}>
+  //         {myArray1[j]}
+  //         {myArray1[j + 1]}
+  //         {myArray1[j + 2]}
+  //       </Grid>
+  //     );
+  //   }
+  //   if(prevState.finalArray !== this.state.finalArray){
+
+  //     this.setState({ finalArray: myArray2 });
+  //   } else {
+  //     console.log("there is no data");
+  //   }
+  // }
 
   render() {
     const { classes } = this.props;
